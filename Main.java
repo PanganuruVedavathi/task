9a0319f8499041b4d242af23023a672fa82f00bc
PanganuruVedavathi <31796854+PanganuruVedavathi@users.noreply.github.com>
2017-09-11 14:03:20 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,8 +2,11 @@ public class Main {
 
 	public static void main(String[] args) {
 
-StudentGroup sg = new StudentGroup(1);
-sg.addFirst();
+StudentGroup sg = new StudentGroup(5);
+Student stud[]=new Student[2];
+stud[0] = new Student(12,"name","06/05/1997",20.5);
+sg.setStudent(stud,0);
+
 		//Youay test that your code works find here
 		//Please check that your code works and has no
 		//compilation problems before to submit
